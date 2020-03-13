5db55ebc95e9143dd663659ed5393079ab59f972
Damir <damir17022000@gmail.com>
2020-03-13 17:52:46 +0300

    init

---
+++ src/main/java/ru/itis/kursach_data/KursachDataApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.kursach_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/kursach_data/KursachDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.kursach_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
